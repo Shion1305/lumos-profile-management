d8e97f47d385e4cc9939c242096496e136349dd2
Shion Ichikawa <shion1305@gmail.com>
2023-05-10 23:59:51 +0900

    ✨ (server/types)User

---
+++ server/types/user.ts
@@ -0,0 +1,18 @@
+export interface User {
+    user_id: number
+    entry_year: number
+    first_name: string
+    last_name: string
+    line_username: string
+    line_service_id: string
+    line_access_token: string
+    line_refresh_token: string
+    line_expires_at: number
+    line_picture_url: string
+    discord_username: string
+    discord_service_id: string
+    discord_access_token: string
+    discord_refresh_token: string
+    discord_expires_at: number
+    discord_picture_url: string
+}
