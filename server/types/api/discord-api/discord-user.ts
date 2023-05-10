38df926990966039fc31740dd4da5b534047f4cf
Shion Ichikawa <shion1305@gmail.com>
2023-05-10 23:50:27 +0900

    ✨ (server/types/api)discord user resp interface

---
+++ server/types/api/discord-api/discord-user.ts
@@ -0,0 +1,12 @@
+export interface DiscordUserResponse {
+    id: string
+    username: string
+    avatar: string
+    discriminator: string
+    public_flags: number
+    flags: number
+    email: string
+    verified: boolean
+    locale: string
+    mfa_enabled: boolean
+}
