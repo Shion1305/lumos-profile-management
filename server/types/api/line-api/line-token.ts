5584c934ca9d36fe4dde659c66a6f17894063c85
Shion Ichikawa <shion1305@gmail.com>
2023-05-11 01:34:00 +0900

    ✨ (api)integration for line login

---
+++ server/types/api/line-api/line-token.ts
@@ -0,0 +1,8 @@
+export interface LineTokenResponse {
+    access_token: string;
+    expires_in: number;
+    id_token: string;
+    refresh_token: string;
+    scope: string;
+    token_type: string;
+}
---
+++ server/types/api/line-api/line-user.ts
@@ -0,0 +1,6 @@
+export interface LineUserProfileResponse {
+    userId: string
+    displayName: string
+    pictureUrl: string
+    statusMessage: string
+}
