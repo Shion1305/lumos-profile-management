9be3f6eeb212ea2009545edb5c0bde3734a01ca1
Shion Ichikawa <shion1305@gmail.com>
2023-05-11 01:24:14 +0900

    ✨ (types)UserProfile

---
+++ server/types/user_profile.ts
@@ -0,0 +1,9 @@
+export interface UserProfile {
+    first_name: string
+    last_name: string
+    entry_year: number
+    discord_username: string
+    discord_picture_url: string
+    line_username: string
+    line_picture_url: string
+}
