64462d15525e3e5ad438ff7e8ca4fff4e7e4750f
Shion Ichikawa <shion1305@gmail.com>
2023-05-10 22:56:01 +0900

    ✨ nuxt.config.tsに必要な環境変数の追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+    ssr: false,
+    runtimeConfig: {
+        line: {
+            clientID: process.env.LINE_CLIENT_ID,
+            callbackURI: process.env.LINE_CALLBACK_URI,
+            clientSecret: process.env.LINE_CLIENT_SECRET
+        },
+        firebase_admin: {
+            certPath: process.env.FIREBASE_ADMIN_CERT_PATH
+        },
+        jwt: {
+            secret: process.env.JWT_SECRET
+        },
+        discord: {
+            clientID: process.env.DISCORD_CLIENT_ID,
+            callbackURI: process.env.DISCORD_CALLBACK_URI,
+            clientSecret: process.env.DISCORD_CLIENT_SECRET
+        }
+    }
 })
