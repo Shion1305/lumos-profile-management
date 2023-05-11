02a92982fccc233b5ea487f8f1a4611a43572196
Shion Ichikawa <shion1305@gmail.com>
2023-05-11 18:38:12 +0900

    🐛 (server/api)Mutateで余計なリクエストが反映されるバグ

--- server/api/mutate.ts
+++ server/api/mutate.ts
@@ -10,15 +10,23 @@ export default defineEventHandler(async (event) => {
         return sendError(event, new Error("invalid token"))
     }
     // get body as MutateRequest
-    const request: MutateRequest = await readBody(event) as MutateRequest
+    const {first_name, last_name, student_id} = await readBody(event) as MutateRequest
     const snapshot = await db.collection("users").doc(event.context.userID).get()
     if (!snapshot.exists) {
         return sendError(event, new Error("user not found"))
     }
-
-
+    const updateData = {} as any
+    if (first_name) {
+        updateData.first_name = first_name
+    }
+    if (last_name) {
+        updateData.last_name = last_name
+    }
+    if (student_id) {
+        updateData.student_id = student_id
+    }
     const opResult = await db.collection("users").doc(event.context.userID)
-        .update(request as any)
+        .update(updateData)
     if (!opResult) {
         return sendError(event, new Error("update failed"))
     }
