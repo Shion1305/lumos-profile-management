a3aeeac1dc21c37714ae0b5904f93fbdfe22f368
Shion Ichikawa <shion1305@gmail.com>
2023-05-11 16:16:01 +0900

    ✨ (page)index

---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+const authURL = useRuntimeConfig().discord.authUrl
+</script>
+
+<template>
+    <div id="base-frame">
+        <div id="header-title">Lumos メンバー管理</div>
+        <hr>
+        <a id="discord-auth"
+           :href="authURL">
+            <img src="@/assets/imgs/discord_logo.svg" alt="Discord">
+            でログイン
+        </a>
+    </div>
+</template>
+
+<style scoped>
+#base-frame {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+hr {
+    width: 100%;
+    border: 2px solid #fff3db;
+    border-radius: 2px;
+    margin: 20px 0;
+}
+
+#header-title {
+    font-size: min(7vw, 30px);
+}
+
+#discord-auth {
+    margin: 0 auto;
+    width: fit-content;
+    color: #fff;
+    padding: 20px;
+    text-align: center;
+    display: flex;
+    background: #5865f2;
+    text-decoration: none;
+    flex-direction: column;
+    border-radius: 15px;
+
+    > img {
+        width: fit-content;
+        height: 40px;
+    }
+
+    font-size: 1.2em;
+    font-weight: bold;
+
+}
+</style>
