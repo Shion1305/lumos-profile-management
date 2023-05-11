e775992d4f0d4bd92c0efca358ca3aff85b4e631
Shion Ichikawa <shion1305@gmail.com>
2023-05-11 18:42:43 +0900

    💄 (layout)アニメーションの追加・レスポンシブ改良

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,9 @@
             <header>
                 <img src="@/assets/imgs/lumos_logo.png" id="logo"/>
             </header>
-            <slot/>
+            <div id="slot">
+                <slot/>
+            </div>
         </div>
     </div>
 </template>
@@ -16,14 +18,48 @@
   width: 100lvw;
   height: 100lvh;
   display: flex;
+  overflow: hidden;
 
   > div {
-    padding: 50px;
+    padding: 30px;
+    box-sizing: border-box;
     max-width: 100%;
     margin: auto;
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+}
+
+// add fade in animation to logo
+#logo {
+  opacity: 0;
+  animation: fadein ease-out .2s .5s forwards
+}
+
+#slot {
+  max-width: 100%;
+  opacity: 0;
+  animation: simple-fadein ease-out .2s .5s forwards
+}
+
+@keyframes fadein {
+  from {
+    opacity: 0;
+    transform: translateY(30%);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0%);
+  }
+}
+
+@keyframes simple-fadein {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
   }
 }
 
