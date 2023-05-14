19994c11c0be9bef95e5b13a8b3f2e8e501dbb72
Shion Ichikawa <shion1305@gmail.com>
2023-05-14 21:21:28 +0900

    💄 (page/admin)nickname欄を追加

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -13,25 +13,31 @@ console.log(members)
 
 <template>
     <h2>Members List</h2>
-    <table id="data-table">
-        <tr>
-            <th>名前</th>
-            <th>学籍番号</th>
-            <th>Discord</th>
-            <th>LINE</th>
-        </tr>
-        <tr v-for="m in members">
-            <td>{{ m.last_name }} {{ m.first_name }}</td>
-            <td>{{ m.student_id }}</td>
-            <td>
-                <img :src="m.discord_picture_url" alt="">
-                {{ m.discord_username }}
-            </td>
-            <td><img :src="m.line_picture_url" alt="">
-                {{ m.line_username }}
-            </td>
-        </tr>
-    </table>
+    <div id="table-area">
+        <table id="data-table">
+            <tr>
+                <th>名前</th>
+                <th>学籍番号</th>
+                <th>Discord</th>
+                <th>Discord Nickname</th>
+                <th>LINE</th>
+            </tr>
+            <tr v-for="m in members">
+                <td>{{ m.last_name }} {{ m.first_name }}</td>
+                <td>{{ m.student_id }}</td>
+                <td>
+                    <img :src="m.discord_picture_url" alt="">
+                    {{ m.discord_username }}
+                </td>
+                <td>
+                    {{ m.discord_nickname}}
+                </td>
+                <td><img :src="m.line_picture_url" alt="">
+                    {{ m.line_username }}
+                </td>
+            </tr>
+        </table>
+    </div>
 </template>
 
 <style scoped lang="scss">
@@ -39,7 +45,14 @@ h2 {
   text-align: center;
 }
 
+#table-area {
+  max-width: 100%;
+  overflow-x: auto;
+}
+
 #data-table {
+  max-width: 90vw;
+  overflow-x: scroll;
   border: 1px solid #e2e2e2;
   border-collapse: collapse;
 
