13fb95a8f5bb05bd6804ac5b097eb025f7d46a4f
Shion Ichikawa <shion1305@gmail.com>
2023-06-18 01:18:03 +0900

    🎨 二重if文の解消

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -3,13 +3,9 @@ import { verifyToken } from '~/server/pkg/jwt'
 export default defineEventHandler((event) => {
     const token = getCookie(event, 'authToken')
     if (token) {
-        if (token) {
-            const auth = verifyToken(token)
-            if (auth) {
-                event.context.userID = auth.id
-            }
-        } else {
-            console.log('No Bearer Token found')
+        const auth = verifyToken(token)
+        if (auth) {
+            event.context.userID = auth.id
         }
     } else {
         console.log('No Authorization header')
