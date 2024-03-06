6496659c7142520ebf1ddf4ec9f715bdd2482527
Shion Ichikawa <shion1305@gmail.com>
2024-03-07 02:37:35 +0900

    🔧 enable ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/google-fonts'],
-  ssr: false,
+  ssr: true,
   runtimeConfig: {
     public: {
       line: {
