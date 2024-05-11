cdf5d650165e7d2635ea33c358a7911283b83ff2
Shion Ichikawa <shion1305@gmail.com>
2024-05-12 03:07:27 +0900

    ✨ admin page with v-data-table

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,9 +2,26 @@
 import axios from 'axios'
 import type { UserProfile } from '~/types/user_profile'
 
-const members: UserProfile[] = await axios
+const members = await axios
   .get<UserProfile[]>('/api/admin/members')
-  .then((res) => res.data)
+  .then((res) => {
+    const tableData = []
+    for (const m of res.data) {
+      tableData.push({
+        name: `${m.last_name} ${m.first_name}`,
+        student_id: m.student_id,
+        discord_on_server: m.discord_on_server,
+        discord_member_role: m.discord_member_role,
+        line: m.line_username,
+        line_img: m.line_picture_url,
+        discord: m.discord_username,
+        discord_nick: m.discord_nickname,
+        discord_img: m.discord_picture_url,
+        has_access: m.has_access
+      })
+    }
+    return tableData
+  })
   .catch((error) => {
     console.log('error in getting members', error)
     return []
@@ -14,72 +31,68 @@ if (members.length === 0) useRouter().push('/profile')
 
 <template>
   <h2>Members List</h2>
-  <div id="table-area">
-    <table id="data-table">
-      <tr>
-        <th>名前</th>
-        <th>学籍番号</th>
-        <th>LINE</th>
-        <th>Discord</th>
-        <th>Joined</th>
-        <th>Nickname</th>
-        <th>Role</th>
-      </tr>
-      <tr v-for="m in members as UserProfile[]">
-        <td>{{ m.last_name }} {{ m.first_name }}</td>
-        <td>{{ m.student_id }}</td>
-        <td>
-          <img :src="m.line_picture_url" alt="" />
-          {{ m.line_username }}
-        </td>
-        <td>
-          <img :src="m.discord_picture_url" alt="" />
-          {{ m.discord_global_name ?? m.discord_username }}
-        </td>
-        <td>
-          {{ m.discord_on_server ? '✅' : '❌' }}
-        </td>
-        <td>
-          {{ m.discord_nickname }}
-        </td>
-        <td>
-          {{ m.discord_member_role ? '✅' : '❌' }}
-        </td>
-      </tr>
-    </table>
-  </div>
+  <v-data-table
+    class="rounded-lg"
+    :headers="[
+      { title: '名前', key: 'name' },
+      { title: '学籍番号', key: 'student_id' },
+      { title: 'LINE', key: 'line' },
+      { title: 'Discord', key: 'discord' },
+      { title: 'Admin', key: 'has_access' }
+    ]"
+    :items="members"
+    :items-per-page="-1"
+  >
+    <template v-slot:header.discord>
+      <div class="text-center font-weight-bold text-subtitle-1">Discord</div>
+    </template>
+    <template v-slot:header.line>
+      <div class="text-center font-weight-bold text-subtitle-1">LINE</div>
+    </template>
+    <template v-slot:item.line="{ item }">
+      <div class="d-flex flex-row pa-1 align-center">
+        <v-img
+          :src="item.line_img"
+          class="rounded-circle flex-grow-0 mr-2"
+          height="36"
+          width="36"
+        />
+        <div class="line-label">{{ item.line }}</div>
+      </div>
+    </template>
+    <template v-slot:item.discord="{ item }">
+      <div class="d-flex flex-row pa-1 align-center">
+        <v-img
+          :src="item.discord_img"
+          class="rounded-circle flex-grow-0 mr-2"
+          height="36"
+          width="36"
+        />
+        <div class="discord-label">{{ item.discord }}</div>
+        <div class="discord-label">{{ item.discord_nick }}</div>
+      </div>
+    </template>
+  </v-data-table>
 </template>
 
 <style lang="scss" scoped>
-h2 {
-  text-align: center;
+.line-label {
+  width: 6rem;
+  font-size: 0.8rem;
+  display: -webkit-box;
+  overflow: hidden;
+  line-clamp: 2;
+  -webkit-line-clamp: 2;
+  text-overflow: ellipsis;
 }
 
-#table-area {
-  max-width: 100%;
-  overflow-x: auto;
-}
-
-#data-table {
-  max-width: 90vw;
-  overflow-x: scroll;
-  border: 1px solid #e2e2e2;
-  border-collapse: collapse;
-
-  th,
-  td {
-    font-size: 1.2em;
-    border: 1px solid #e2e2e2;
-    padding: 0.7em;
-
-    > * {
-      vertical-align: middle;
-    }
-
-    > img {
-      border-radius: 50%;
-      height: 2em;
-    }
-  }
+.discord-label {
+  width: 5rem;
+  font-size: 0.8rem;
+  display: -webkit-box;
+  overflow: hidden;
+  line-clamp: 2;
+  -webkit-line-clamp: 2;
+  text-overflow: ellipsis;
 }
 </style>
